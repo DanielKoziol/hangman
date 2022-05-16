2e276f37417a1dd4b1f7a520038f3756684a0370
DanielKoziol <koziol.daniel@interia.pl>
2022-05-17 00:25:41 +0200

    class added

--- hangman.rb
+++ hangman.rb
@@ -4,7 +4,7 @@ number = 39
 def get_word (number, file)
 count = 0
 
-    until count == number
+until count == number
   p "#{count} #{file.gets}"
   count += 1
     end
@@ -15,16 +15,72 @@ end
 
 word = get_word(number, file)
 
-#def check_word(word)
+def check_word(word)
 p word.length
  until p (6..13).include?(word.length)
     p word
-#else
+
     file.rewind
     number += 1
     word = get_word(number, file)
 end
 p word
-#end
+end
 
 #p words_file
+
+
+module Getting_input
+  def 
+
+
+end
+end
+
+class Game_prep
+  FILE = File.open('english_words.txt', 'r')
+
+  attr_accessor :word
+
+  def initialize
+    @word = ""
+  end
+
+  def get_word (number)
+    count = 0
+    
+    until count == number
+      p "#{count} #{FILE.gets}"
+      count += 1
+        end
+    line = FILE.gets
+    p line
+  end
+
+  def check_word(word, number)
+      until p (6..13).include?(word.length)
+        p word
+        FILE.rewind
+        number += 1
+        word = get_word(number)
+      end
+    self.word = word
+  end
+
+  
+
+    
+end
+
+class Game
+  include Getting_input
+
+  attr_accessor
+  attr_reader
+
+  def initialize(secret_word = "")
+    @secret_word = secret_word
+  end
+
+
+end
